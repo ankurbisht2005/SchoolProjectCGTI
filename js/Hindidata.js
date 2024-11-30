91d17e3b9b749f14f26f822ffe42c7f7f60ede32
ankurbisht2005 <ankurbisht799@gmail.com>
2024-11-30 09:20:44 +0530

    updated-schoolproject

---
+++ js/Hindidata.js
@@ -0,0 +1,29 @@
+const videoData = [
+    {
+        title: "Bandar Mama Pahan Pajama",
+        thumbnail: "../images/Hindi Rhymes/Bandar Mama Pahan Pajama.png",
+        source: "../Videos/Hindi Rhymes/Bandar Mama Pahan Pajama.mp4",
+    },
+    {
+        title: "Billi Mausi Billi Mausi Kaho Kahan Se Aayi Ho",
+        thumbnail: "../images/Hindi Rhymes/Billi Mausi Billi Mausi Kaho Kahan Se Aayi Ho.png",
+        source: "../Videos/Hindi Rhymes/Billi Mausi Billi Mausi Kaho Kahan Se Aayi Ho.mp4",
+    },
+    {
+        title: "Chunnu Munnu",
+        thumbnail: "../images/Hindi Rhymes/Chunnu Munnu.png",
+        source: "../Videos/Hindi Rhymes/Chunnu Munnu.mp4",
+    },
+    {
+        title: "Hatti Raja Kahan Chale",
+        thumbnail: "../images/Hindi Rhymes/Hatti Raja Kahan Chale.png",
+        source: "../Videos/Hindi Rhymes/Hatti Raja Kahan Chale.mp4",
+    },
+    {
+        title: "Varnamala Geet Hindi Alphabet",
+        thumbnail: "../images/Hindi Rhymes/Varnamala Geet Hindi Alphabet.png",
+        source: "../Videos/Hindi Rhymes/Varnamala Geet Hindi Alphabet.mp4",
+    },
+    
+    // Add more video objects up to 20
+];
